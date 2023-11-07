6fcc0b9c38a73e90625df396b7c5aea21dc86acf
Sergio Andrés SÁNCHEZ <103612998+SergioAndresSanchezTdfEdu@users.noreply.github.com>
2023-11-07 20:01:31 -0300

    a clever-cloud

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,12 +7,20 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
   imports: [
     TypeOrmModule.forRoot({
+      /* Local */
+      // type: 'mysql',
+      // host: 'localhost',
+      // port: 3306,
+      // username: 'root',
+      // password: 'Jobara123',
+      // database: 'lista-prueba',
+      /* Clever-Cloud */
       type: 'mysql',
-      host: 'localhost',
+      host: 'bs0bivedcuimbr1vtslq-mysql.services.clever-cloud.com',
       port: 3306,
-      username: 'root',
-      password: 'Jobara123',
-      database: 'lista-prueba',
+      username: 'ujtahbqmmxxh02dv',
+      password: 'nBfUPTPFyJbRAkpUAvtp',
+      database: 'bs0bivedcuimbr1vtslq',
       entities: [__dirname + '/**/*.entity{.ts,.js}'],
       synchronize: false, // En true crea la tabla, luego pasar a false para que no borre lo cargado en la tabla
     }),    
